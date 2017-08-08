d60176289ecf873ebbf81ef7c374289249df0f57
Ruby Kim <rubyk70231@gmail.com>
2017-08-08 14:10:26 +0200

    update routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/pages/tour', to: 'pages#tour'
+
+  resource :account, only: [:show, :edit, :update]
+
+  resources :trips, only: [:index] do
+    member do
+      patch :cancel
+    end
+  end
+
+  resources :scooters, only: [:index, :new, :create, :show] do
+    resources :reservations, only: [:new, :create]
+  end
+  resources :reservations, only: [:index] do
+    member do
+        patch :approve
+        patch :deny
+    end
+  end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
