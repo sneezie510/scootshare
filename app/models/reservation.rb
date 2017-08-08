52d6d2be798d2586fc493b2d4b1595b2d0c722c8
Sandi Huynh <shuynh1300@yahoo.com>
2017-08-08 11:48:15 +0200

    add validations and test models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :scooter
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/scooter.rb
+++ app/models/scooter.rb
@@ -1,3 +1,6 @@
 class Scooter < ApplicationRecord
   belongs_to :user
+  has_many :reservations
+  validates :location, presence: true
+  validates :availability, inclusion: { in: [true, false] }
 end
