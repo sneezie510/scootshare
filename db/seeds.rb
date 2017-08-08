0f41250d71cba454d71c903fe33544c2d7aab02b
Ash Madi <a.madi27@gmail.com>
2017-08-08 13:45:33 +0200

    Create Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  user = User.new({ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, address: Faker::Address.city, avatar: Faker::Avatar.image, password: 'ASDFLJASDJF'})
+  if user.save
+    puts "#{user.first_name} saved"
+  end
+end
+
+10.times do
+  Scooter.create({ user: User.all.sample, make: Faker::Vehicle.manufacture, model: Faker::Vehicle.vin, location: Faker::Address.city, availability: true, picture: Faker::LoremPixel.image})
+end
