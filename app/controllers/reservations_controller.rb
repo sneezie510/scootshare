718caf078d610c0c2cc0170e43ef0a1c54ae43b4
Ruby Kim <rubyk70231@gmail.com>
2017-08-09 15:58:37 +0200

    dynamic dates and pricing on scooter show

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -12,8 +12,13 @@ class ReservationsController < ApplicationController
   # end
 
   def create
-    # reservation = Reservation.create()
-    if reservation.save
+    @reservation = Reservation.new(reservation_params)
+    @reservation.user = current_user
+    @scooter = Scooter.find(params[:scooter_id])
+    @reservation.scooter = @scooter
+    @reservation.total_price = ((@reservation.end_date - @reservation.start_date).to_i + 1) * @scooter.price
+    @reservation.status = "pending"
+    if @reservation.save
       redirect_to trips_path
     else
       render :new
@@ -21,7 +26,15 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @scooter = Scooter.find(params[:scooter_id])
     @reservation = Reservation.new
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+
   end
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,13 @@ class Reservation < ApplicationRecord
   belongs_to :scooter
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validate :end_must_be_after_start
+
+  private
+
+  def end_must_be_after_start
+    if start_date > end_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
 end
